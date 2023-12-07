b2f3fe40df129eff45ad79a553038ded832d551e
wotosts <tosts522@gmail.com>
2023-12-08 00:20:14 +0900

    chore: supabase 적용

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'feature/signin/splash_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://slrafytgdorwxxdcaasi.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNscmFmeXRnZG9yd3h4ZGNhYXNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEwNzAwNzcsImV4cCI6MjAxNjY0NjA3N30.rgdxCqjZ-uxcMRN1wI2waxhHqPsBWg0FETPJ5uBOWmo',
+  );
+
   runApp(const ProviderScope(child: MyApp()));
 }
+
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
