ea68a4001fae60cdaf47834ea4c8baa5f17e3616
wotosts <tosts522@gmail.com>
2023-12-13 23:46:07 +0900

    feat: Gender 추가

--- lib/model/animal.dart
+++ lib/model/animal.dart
@@ -5,12 +5,22 @@ part 'animal.freezed.dart';
 
 @freezed
 class Animal with _$Animal {
-  factory Animal({
-          required final int id,
-          required final String name,
-          required final String species,
-          required final DateTime birth,
-          required final String gender,
-          final String? note,
-          final String? imgUrl}) = _Animal;
+  factory Animal(
+      {required final int id,
+      required final String name,
+      required final String species,
+      required final DateTime birth,
+      required final String gender,
+      final String? note,
+      final String? imgUrl}) = _Animal;
+}
+
+enum Gender {
+  male('수컷'),
+  female('암컷'),
+  neutral('중성');
+
+  final String korean;
+
+  const Gender(this.korean);
 }
