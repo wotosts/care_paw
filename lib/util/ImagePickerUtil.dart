1964fbbade325ab2eddba0c2792f2786239d32db
wotosts <tosts522@gmail.com>
2023-12-14 19:12:20 +0900

    feat: ImagePickerUtil 생성

---
+++ lib/util/ImagePickerUtil.dart
@@ -0,0 +1,20 @@
+import 'package:image_picker/image_picker.dart';
+
+class ImagePickerUtil {
+  ImagePickerUtil._internal();
+
+  factory ImagePickerUtil.getInstance() => _instance;
+  static final ImagePickerUtil _instance = ImagePickerUtil._internal();
+
+  final ImagePicker _picker = ImagePicker();
+
+  Future<XFile?> selectSingleImage() async {
+    final XFile? picked = await _picker.pickImage(source: ImageSource.gallery);
+    return picked;
+  }
+
+  Future<List<XFile?>> selectImages() async {
+    final picked = await _picker.pickMultiImage();
+    return picked;
+  }
+}
