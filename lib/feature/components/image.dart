47a3678043b2f1e9ab0f8b17f24a02e1fee1e2ad
wotosts <tosts522@gmail.com>
2023-12-19 23:35:56 +0900

    ui: supabase 이미지 노출용 SBImage 추가

---
+++ lib/feature/components/image.dart
@@ -0,0 +1,41 @@
+import 'package:care_paw/util/EmptyExtensions.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:path/path.dart';
+
+import '../../data/di.dart';
+import '../../data/image_repository.dart';
+
+/// supabase image
+class SBImage extends ConsumerWidget {
+  final String url;
+
+  const SBImage({required this.url});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var imageRepository = ref.read(imageRepositoryProvider);
+
+    return Card(
+        elevation: 0,
+        color: Theme.of(context).colorScheme.secondaryContainer,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        clipBehavior: Clip.antiAlias,
+        child: Center(
+            child: FutureBuilder(
+                future: imageRepository.getUrl(Bucket.animal, basename(url)),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: Image.network(snapshot.data.orEmpty()).image)));
+                  } else {
+                    return Icon(Icons.pix,
+                        color:
+                            Theme.of(context).colorScheme.onSecondaryContainer);
+                  }
+                })));
+  }
+}
