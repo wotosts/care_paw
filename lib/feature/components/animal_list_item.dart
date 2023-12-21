7209e0a0eef959ec7927815d04d2ff6ac6f1d5d2
wotosts <tosts522@gmail.com>
2023-12-21 23:54:37 +0900

    ui: 입원목록 item에 SBImage 적용

--- lib/feature/components/animal_list_item.dart
+++ lib/feature/components/animal_list_item.dart
@@ -1,6 +1,12 @@
+import 'package:care_paw/data/di.dart';
+import 'package:care_paw/data/image_repository.dart';
+import 'package:care_paw/feature/components/image.dart';
 import 'package:care_paw/model/hospitalization.dart';
 import 'package:care_paw/feature/components/badge.dart';
+import 'package:care_paw/util/EmptyExtensions.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:path/path.dart';
 
 class AnimalListItem extends StatelessWidget {
   final Hospitalization item;
@@ -12,7 +18,6 @@ class AnimalListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
 
-    // todo make clickable card
     return Card(
         margin: EdgeInsets.zero,
         color: theme.colorScheme.surface,
@@ -24,43 +29,43 @@ class AnimalListItem extends StatelessWidget {
         elevation: 0.0,
         child: InkWell(
           customBorder:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
           onTap: onClick,
           child: Container(
             padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
             child: Row(
               mainAxisSize: MainAxisSize.max,
               children: [
-                const SizedBox(
-                    width: 48,
-                    height: 48,
-                    child: Placeholder(color: Colors.blueGrey)),
+                SizedBox(
+                    width: 56,
+                    height: 56,
+                    child: SBImage(url: item.animal.imgUrl.orEmpty())),
                 const Padding(padding: EdgeInsets.only(left: 14)),
                 Expanded(
                     child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          item.animal.name,
-                          style: theme.textTheme.titleMedium
-                              ?.copyWith(fontWeight: FontWeight.bold),
+                        Row(
+                          children: [
+                            Text(
+                              item.animal.name,
+                              style: theme.textTheme.titleMedium
+                                  ?.copyWith(fontWeight: FontWeight.bold),
+                            ),
+                            const Padding(padding: EdgeInsets.only(left: 4)),
+                            CPBadge(text: item.animal.species.korean)
+                          ],
                         ),
-                        const Padding(padding: EdgeInsets.only(left: 4)),
-                        CPBadge(text: item.animal.species)
+                        Text(
+                          item.hospitalizationDateString(),
+                          style: theme.textTheme.bodySmall
+                              ?.copyWith(color: const Color(0xFF79747E)),
+                        ),
+                        Text(item.animal.note ?? '',
+                            style: theme.textTheme.bodySmall
+                                ?.copyWith(color: const Color(0xFF79747E)))
                       ],
-                    ),
-                    Text(
-                      item.hospitalizationDateString(),
-                      style: theme.textTheme.bodySmall
-                          ?.copyWith(color: const Color(0xFF79747E)),
-                    ),
-                    Text(item.animal.note ?? '',
-                        style: theme.textTheme.bodySmall
-                            ?.copyWith(color: const Color(0xFF79747E)))
-                  ],
-                )),
+                    )),
                 IconButton(
                     onPressed: () {},
                     icon: Icon(
